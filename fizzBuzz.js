d67b995422903276148e81a1e8ca62613cb28e0b
[WCS/NTS] Karl Morisset <90033508+karlmorisset@users.noreply.github.com>
2022-03-08 00:35:46 +0100

    fizzbuzz ok

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,9 @@
-function fizzBuzz(){
-  // Start here
+function fizzBuzz(number){
+  if (number % 3 == 0 && number % 5 == 0) return "fizzbuzz"
+  if (number % 3 == 0) return "fizz"
+  if (number % 5 == 0) return "buzz"
+
+  return number
 };
 
 module.exports = fizzBuzz;
